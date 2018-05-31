ed480592b637ffed4ecffecdac0e8b4260d3d73e
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-05-31 16:58:37 -0300

    fix bundle dashboard

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Uspdev\BootstrapDashboardBundle\BootstrapDashboardBundle::class => ['all' => true],
+    Uspdev\BootstrapDashboardBundle\UspdevBootstrapDashboardBundle::class => ['all' => true],
 ];
